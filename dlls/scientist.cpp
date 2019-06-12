0344e9ebabfdc5b4bde4924cb64dc3aa995d46e0
Step4enko <48833228+Step4enko@users.noreply.github.com>
2019-06-12 19:22:24 +0300

    Update scientist.cpp

--- dlls/scientist.cpp
+++ dlls/scientist.cpp
@@ -1,17 +1,20 @@
 /***
 *
-*	Copyright (c) 2016-2020, UPTeam. All rights reserved.
+*   Copyright (c) 1996-2001, Valve LLC. All rights reserved.
 *	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
+*   This product contains software technology licensed from Id 
+*   Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*   All Rights Reserved.
 *
-*   This code is made by UPTeam (Half-Life: Ultimate Point developers)
-*   Coders: Step4enko/LambdaGames22
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
 *
 *   https://www.moddb.com/mods/half-life-ultimate-point
 *
 ****/
+
 
 //=========================================================
 // Scientist
